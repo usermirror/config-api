e28b0ca2042159b9fb043080f0f6d600a39018d5
Fouad Matin <m@fouad.org>
2018-09-06 14:51:44 -0700

    update(migrations): add namespace_tokens table

--- pkg/migrations/postgres/001_create_namespace_tokens.go
+++ pkg/migrations/postgres/001_create_namespace_tokens.go
@@ -4,17 +4,17 @@ import (
 	"github.com/usermirror/config-api/pkg/migrations/schema"
 )
 
-var createNamespaces = schema.Migration{
-	Name:    "create_namespaces",
+var createNamespaceTokens = schema.Migration{
+	Name:    "create_namespace_tokens",
 	Version: 2,
 	Up: func() string {
-		return `CREATE TABLE IF NOT EXISTS "public"."namespaces" (
+		return `CREATE TABLE IF NOT EXISTS "public"."namespace_tokens" (
 	"namespace_id" bytea,
 	"write_tokens" bytea,
 	PRIMARY KEY (namespace_id)
 );`
 	},
 	Down: func() string {
-		return `DROP TABLE "public"."namespaces";`
+		return `DROP TABLE "public"."namespace_tokens";`
 	},
 }
