c1e743f7a87a74f2a214835dc583e0161ccd8b95
Dan Gillespie <dan@redspread.com>
2018-09-14 17:12:40 -0700

    Terraform deploy (#5)
    Terraform modules for generic Kubernetes and GCP deploys

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+// defaults requiring interpolation
+locals {
+  render_dir    = "${var.render_dir=="" ? local.default_render_dir : var.render_dir}"
+  config_path   = "${var.config_path=="" ? local.default_config_path : var.config_path}"
+  manifests_dir = "${var.manifests_dir=="" ? local.default_manifests_dir : var.manifests_dir}"
+
+  default_render_dir    = "${dirname(path.module)}/dist/manifests"
+  default_config_path   = "${dirname(path.module)}/manifests/config.yaml"
+  default_manifests_dir = "${dirname(path.module)}/manifests"
+
+  last = "${element(list("", var.last_resource), 0)}"
+}
+
+module "config" {
+  source = "git::https://github.com/helpusersvote/terraform-kubernetes-helpusersvote.git//modules/config"
+
+  components   = ["config-api"]
+  render_dir   = "${local.render_dir}"
+  config       = "${local.config_path}${local.last}" // TODO: remove once module dependency can be improved
+  manifest_dir = "${local.manifests_dir}"
+}
+
+module "kubernetes" {
+  source = "git::https://github.com/helpusersvote/terraform-kubernetes-helpusersvote.git//modules/kubernetes"
+
+  manifest_dirs = "${module.config.dirs}"
+  kubeconfig    = "${var.kubeconfig}"
+  last_resource = "${join(",", module.config.manifest_state)}"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "render_dir" {
+  description = "Path to directory where templated manifests can be outputted (defaults to path within module)"
+  type        = "string"
+  default     = ""
+}
+
+variable "config_path" {
+  description = "Path to ConfigMap describing configuration for config-api (defaults to path within module)"
+  type        = "string"
+  default     = ""
+}
+
+variable "manifests_dir" {
+  description = "Directory containing manifests used for deployment (defaults to path within module)"
+  type        = "string"
+  default     = ""
+}
+
+variable "kubeconfig" {
+  description = "Path to kubeconfig used to authenticate with Kubernetes API server"
+  type        = "string"
+}
+
+variable "last_resource" {
+  description = "Allows dependency to be expressed to module"
+  type        = "string"
+  default     = ""
+}
